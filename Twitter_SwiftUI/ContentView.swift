a8a75cf51c6b5e155a1f3d853799c0ea62729ae8
Ankit Saxena <ankitatdelhii@gmail.com>
2021-05-15 23:53:18 +0530

    Initial Commit

---
+++ Twitter_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Twitter_SwiftUI
+//
+//  Created by Ankit Saxena on 15/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Twitter_SwiftUI/Twitter_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_SwiftUIApp.swift
+//  Twitter_SwiftUI
+//
+//  Created by Ankit Saxena on 15/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
